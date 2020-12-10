89ddc5936fdfc7e110dd36eda4403b193db3008c
RagvDest <rennygorozabel@hotmail.com>
2020-12-10 11:05:00 -0500

    Mover lectura de propiedad a metodo privado

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,7 +1,9 @@
 package com.clearminds.impl;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 import com.clearminds.excepciones.InstanceException;
@@ -14,11 +16,8 @@ public class PersonaManager {
 	public PersonaManager() throws InstanceException {
 		super();
 		try {
-			Properties p = new Properties();
-			File f=new File("config.properties");
-			p.load(new FileReader("config.properties"));
-			Class<?> clase = Class.forName(p.getProperty("instancia"));
-			this.srv = (ServicioPersona)clase.newInstance();
+			
+			this.srv = (ServicioPersona)obtenerClasePropiedad().newInstance();
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new InstanceException("Error al obtener una instancia de ServicioPersona");
@@ -27,6 +26,13 @@ public class PersonaManager {
 	
 	public void insertarPersona(Persona p){
 		srv.insertar(p);
+	}
+	
+	private Class<?> obtenerClasePropiedad() throws FileNotFoundException, IOException, ClassNotFoundException{
+		Properties p = new Properties();
+		File f=new File("config.properties");
+		p.load(new FileReader("config.properties"));
+		return Class.forName(p.getProperty("instancia"));
 	}
 	
 }
