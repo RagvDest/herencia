d995faa6c9aaffcfb0cce4860cbb7c794ede154b
RagvDest <rennygorozabel@hotmail.com>
2020-12-10 10:37:59 -0500

    Config Properties and testManager

--- src/test/java/com/clearminds/rgv/herenciaEvaluacion/TestManager.java
+++ src/test/java/com/clearminds/rgv/herenciaEvaluacion/TestManager.java
@@ -1,5 +1,11 @@
 package com.clearminds.rgv.herenciaEvaluacion;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.excepciones.InstanceException;
 import com.clearminds.impl.PersonaManager;
 import com.clearminds.model.Persona;
@@ -8,9 +14,16 @@ public class TestManager {
 
 	public static void main(String[] args) {
 		try {
-			PersonaManager pManager = new PersonaManager("com.clearminds.impl.ServicioPersonaArchivos");
+			Properties p = new Properties();
+			File f=new File("config.properties");
+			p.load(new FileReader("config.properties"));
+			PersonaManager pManager = new PersonaManager(p.getProperty("instancia"));
 			pManager.insertarPersona(new Persona("Portoviejo","Quito","Ecuatoriana"));
 		} catch (InstanceException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
